66b37a3ef5adbe0f7ea44718adbea69f144b1565
Bilgisayar Muhendisligi Dersleri <74094387+bmdersleri@users.noreply.github.com>
2022-06-05 20:46:40 +0300

    b

--- Öğrenci_Çalışmaları/1911404039_HasanEmreAlkan/vs_code_gelirgider_takip/android/app/src/main/kotlin/com/example/vs_code_gelirgider_takip/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.vs_code_gelirgider_takip
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Öğrenci_Çalışmaları/1911404039_HasanEmreAlkan/vs_code_gelirgider_takip/lib/constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-TextStyle kMetinStili = const TextStyle(
-  color: Colors.black54,
-  fontSize: 20,
-  fontWeight: FontWeight.w300,
-);
